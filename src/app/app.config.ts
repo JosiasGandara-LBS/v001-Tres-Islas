5535f4e11ccc11538881a497fc2d91472c9336e9
Josias Gandara <jgandara@lbsplus.mx>
2025-03-05 14:24:07 -0700

    Enviroment and transitions on all pages

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withViewTransitions } from '@angular/router';
 
 import { routes } from './app.routes';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
@@ -8,16 +8,18 @@ import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { getStorage, provideStorage } from '@angular/fire/storage';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { importProvidersFrom } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, provideHttpClient, withFetch } from '@angular/common/http';
 import { DynamicDialogModule } from 'primeng/dynamicdialog';
+import { firebaseConfig } from 'src/environment/firebase.config';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes), 
-    provideFirebaseApp(() => initializeApp({"projectId":"v001-tres-islas","appId":"1:960197070111:web:290042c5ed81f43961742a","storageBucket":"v001-tres-islas.firebasestorage.app","apiKey":"AIzaSyAmh1UUiezJub6CBabwvCDND6PN_nuDdys","authDomain":"v001-tres-islas.firebaseapp.com","messagingSenderId":"960197070111","measurementId":"G-319X4NLHW4"})), 
-    provideAuth(() => getAuth()), 
-    provideFirestore(() => getFirestore()), 
-    provideFirebaseApp(() => initializeApp({"projectId":"v001-tres-islas","appId":"1:960197070111:web:290042c5ed81f43961742a","storageBucket":"v001-tres-islas.firebasestorage.app","apiKey":"AIzaSyAmh1UUiezJub6CBabwvCDND6PN_nuDdys","authDomain":"v001-tres-islas.firebaseapp.com","messagingSenderId":"960197070111","measurementId":"G-319X4NLHW4"})), 
+    provideRouter(routes, withViewTransitions()),
+	provideHttpClient(withFetch()),
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
     provideStorage(() => getStorage()),
 		importProvidersFrom(BrowserAnimationsModule),
 		importProvidersFrom(HttpClientModule),
---
+++ src/environment/firebase.config.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+	projectId: 'v001-tres-islas',
+	appId: '1:960197070111:web:290042c5ed81f43961742a',
+	storageBucket: 'v001-tres-islas.appspot.com',
+	apiKey: 'AIzaSyAmh1UUiezJub6CBabwvCDND6PN_nuDdys',
+	authDomain: 'v001-tres-islas.firebaseapp.com',
+	messagingSenderId: '960197070111',
+	measurementId: 'G-319X4NLHW4'
+};
