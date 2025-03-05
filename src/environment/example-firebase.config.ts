de5a0da07829dce085af774e58e2c13ec6a79165
Josias Gandara <jgandara@lbsplus.mx>
2025-03-05 14:30:13 -0700

    Enviroment example

---
+++ src/environment/example-firebase.config.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+	projectId: 'PROJECTID',
+	appId: 'APPID',
+	storageBucket: 'STORAGEBUCKET',
+	apiKey: 'APIKEY',
+	authDomain: 'AUTHDOMAIN',
+	messagingSenderId: 'MESSAGINGSENDERID',
+	measurementId: 'MEASUREMENTID'
+};
