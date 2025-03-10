e67cebb699ac5e5456caac9350cfc69851493860
plizarraga <pabloademir05@gmail.com>
2025-03-10 07:58:50 -0700

    Minos changes to orders-client and products

--- src/app/pages/orders-client/orders-client.component.ts
+++ src/app/pages/orders-client/orders-client.component.ts
@@ -3,11 +3,12 @@ import { RouterModule } from '@angular/router';
 
 import { OrdersClientListComponent } from './components/orders-client-list/orders-client-list.component';
 import { NavbarComponent } from '../home/components/navbar/navbar.component';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-orders-client',
   standalone: true,
-  imports: [RouterModule, NavbarComponent, OrdersClientListComponent],
+  imports: [RouterModule, NavbarComponent, OrdersClientListComponent, ButtonModule],
   templateUrl: './orders-client.component.html',
   styleUrl: './orders-client.component.scss'
 })
