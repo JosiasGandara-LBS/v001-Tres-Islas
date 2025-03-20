2fce5e026c686978af3041b4d01b88ea87035f93
unknown <adrianloshjo@gmail.com>
2025-03-19 20:26:41 -0700

    Maximo platillos 9

--- src/app/pages/home/components/modal-item/modal-item.component.ts
+++ src/app/pages/home/components/modal-item/modal-item.component.ts
@@ -49,7 +49,7 @@ export class ModalItemComponent implements OnInit, OnDestroy {
 
 	// Llamar a la función de incrementar cantidad
 	incrementQuantity(): void {
-		this.quantity++;
+		if(this.quantity < 9) this.quantity++;
 	}
 
 	// Llamar a la función de decrementar cantidad
