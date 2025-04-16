ece1370b2c0d8d3a9a8bf3f7ad9f1e752148e58d
plizarraga <p.lizarraga@lbsplus.mx>
2025-04-15 23:41:50 -0700

    feat: agregada gestion de mesas en admin

--- src/app/pages/admin/configs/configs.component.ts
+++ src/app/pages/admin/configs/configs.component.ts
@@ -1,14 +1,15 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import { Component, effect, inject, OnInit, signal } from '@angular/core';
 import { InputSwitchModule } from 'primeng/inputswitch';
 import { CommonModule } from '@angular/common';
-import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { KitchenStatusService } from '@core/services/kitchen-status.service';
 import Swal from 'sweetalert2';
 import { PromotionsService } from '@core/services/promotions.service';
+import { DividerModule } from 'primeng/divider';
 @Component({
   selector: 'app-configs',
   standalone: true,
-  imports: [InputSwitchModule, CommonModule, ReactiveFormsModule],
+  imports: [InputSwitchModule, CommonModule, ReactiveFormsModule, DividerModule],
   templateUrl: './configs.component.html',
   styleUrl: './configs.component.scss'
 })
@@ -22,10 +23,24 @@ export class ConfigsComponent implements OnInit{
 		ProductDiscountEnabled: [false, [Validators.required]],
 		ProductDiscountStartTime: [null, [Validators.required]],
 		ProductDiscountEndTime: [null, [Validators.required]],
+		tableName: [null],
 	}));
+
+	editTableForm: FormGroup = this.fb.group({
+		name: ['', Validators.required]
+	});
+	editingTableId: number | null = null;
+
+	public tables = signal<any[]>([]);
 
 	ngOnInit(): void {
 		this.getConfigs()
+	}
+
+	constructor() {
+		effect(() => {
+			console.log(this.tables());
+		});
 	}
 
 
@@ -57,6 +72,8 @@ export class ConfigsComponent implements OnInit{
 			}).then(() => {}).catch((error) => {
 				throw new Error('Error al guardar descuentos de productos: ' + error);
 			});
+
+			this.saveTables();
 
 			Swal.fire({
 				title: 'Configuraciones guardadas',
@@ -93,6 +110,7 @@ export class ConfigsComponent implements OnInit{
 				});
 			}
 		});
+		this.getTables();
 	}
 
 	resetConfigs() {
@@ -104,7 +122,72 @@ export class ConfigsComponent implements OnInit{
 			confirmButtonText: 'Aceptar'
 		});
 		this.configsForm().markAsPristine();
+		this.editTableForm.reset();
+		this.editingTableId = null;
 	}
 
+	getTables() {
+		this.kitchenService.getTables().subscribe((tables: any) => {
+			// Ensure that we only set an array
+			this.tables.set(Array.isArray(tables) ? tables : []);
+		});
+	}
+
+	saveTables() {
+		this.kitchenService.saveTables(this.tables()).then(() => {
+			this.editTableForm.reset();
+			this.editingTableId = null;
+		});
+	}
+
+	addTable() {
+		const newTable: string = this.configsForm().get('tableName')?.value || "";
+		if (!newTable) {
+			Swal.fire({
+				title: 'Error',
+				text: 'Por favor ingresa un nombre para la mesa',
+				icon: 'error',
+				confirmButtonText: 'Aceptar'
+			});
+			return;
+		}
+		if (this.tables().includes(newTable.toUpperCase())) {
+			Swal.fire({
+				title: 'Error',
+				text: 'El nombre de la mesa ya existe',
+				icon: 'error',
+				confirmButtonText: 'Aceptar'
+			});
+			this.configsForm().get('tableName')?.setValue(null);
+			return;
+		}
+		const currentTables = this.tables();
+		const updatedTables = [...currentTables, newTable.toUpperCase()];
+		this.tables.set(updatedTables);
+		this.configsForm().get('tableName')?.setValue(null);
+	}
+
+	deleteTable(index: number) {
+		const updatedTables = [...this.tables()];
+		updatedTables.splice(index, 1);
+		this.tables.set(updatedTables);
+		this.editTableForm.reset();
+		this.editingTableId = null;
+	}
+
+	editTable(index: number) {
+		this.editingTableId = index;
+		this.editTableForm.patchValue({ name: this.tables()[index] });
+	}
+
+	updateTable(index: number) {
+		if (this.editTableForm.valid) {
+			const updatedTables = [...this.tables()];
+			updatedTables[index] = this.editTableForm.get('name')?.value;
+			this.tables.set(updatedTables);
+			this.editTableForm.reset();
+			this.editingTableId = null;
+		}
+	}
 
 }
